90846f24b3d033a785fbfc585fa18360a707b9e4
Sanketh Mundas <sankethmundas3@gmail.com>
2026-06-25 23:35:08 -0700

    Add JWT authentication middleware and protected routes

--- nextrole-backend/routes/authRoutes.js
+++ nextrole-backend/routes/authRoutes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const protect = require("../middleware/authMiddleware");
 
 const {
     registerUser,
@@ -6,6 +7,19 @@ const {
 } = require("../controllers/authController");
 
 const router = express.Router();
+
+router.get(
+    "/profile",
+    protect,
+    (req, res) => {
+
+        res.json({
+            message: "Protected Route",
+            userId: req.user
+        });
+
+    }
+);
 
 router.post("/register", registerUser);
 
