d4d63f00c0fe1fb6e76ff66f9fa4fbebc47f5750
Sanketh Mundas <sankethmundas3@gmail.com>
2026-06-25 22:45:55 -0700

    Implemented JWT authentication with user login API

--- nextrole-backend/controllers/authController.js
+++ nextrole-backend/controllers/authController.js
@@ -1,5 +1,67 @@
 const bcrypt = require("bcryptjs");
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+
+const loginUser = async (req, res) => {
+    try {
+
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({
+                message: "Please provide email and password"
+            });
+        }
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({
+                message: "Invalid email or password"
+            });
+        }
+
+        const isMatch = await bcrypt.compare(
+            password,
+            user.password
+        );
+
+        if (!isMatch) {
+            return res.status(400).json({
+                message: "Invalid email or password"
+            });
+        }
+
+        const token = jwt.sign(
+            {
+                userId: user._id
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "7d"
+            }
+        );
+
+        res.status(200).json({
+            message: "Login successful",
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email
+            }
+        });
+
+    } catch (error) {
+
+        console.error(error);
+
+        res.status(500).json({
+            message: "Server Error"
+        });
+
+    }
+};
 
 const registerUser = async (req, res) => {
     try {
@@ -42,5 +104,6 @@ const registerUser = async (req, res) => {
 };
 
 module.exports = {
-    registerUser
+    registerUser,
+    loginUser
 };
